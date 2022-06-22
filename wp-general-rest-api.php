0bb098eebb82419c5c0a562bd287c1d7d7949ca6
claudionhangapc <claudionhangapc@gmail.com>
2022-06-21 22:51:16 -0300

    created definition information

---
+++ wp-general-rest-api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+* Plugin Name:       WP GENERAL REST API
+* Plugin URI:        https://example.com/plugins/the-basics/
+* Description:       Este é um plugin que gera as rotas para obter os dados do site wordpress, permitindo requisições autencticadas usando jwt
+* Version:           1.0.0
+* Requires at least: 5.2
+* Requires PHP:      7.2
+* Author:            Oi.Mark
+* Author URI:        https://author.example.com/
+* License:           GPL v2 or later
+* License URI:       https://oimark.com.br/gpl-2.0.html
+* Update URI:        https://oimark.com.br/
+* Text Domain:       https://oimark.com.br/
+*/
