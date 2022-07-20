a9a2a26eb57ca1a957198cdff36268a345f4061a
claudionhangapc <claudionhangapc@gmail.com>
2022-07-19 21:35:14 -0300

    handler with login part - 1

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,15 +2,31 @@
 
 namespace Controllers;
 
+use Models\UserModel;
+
 class UserController 
-{
+{ 
+  private $userModel;
   
+  function __construct()
+  {
+    $this->userModel = new UserModel();
+  }
+
   public function login($request){
 
     $username = $request['username'];
     $password = $request['password'];
     
-    return rest_ensure_response($request['username']);
+    $result = $this->userModel->login($username, $password);
+
+    // retorna o erro se usuario não conseguir logar
+    if(!isset($result->data)){
+      return rest_ensure_response($result);
+    }
+
+    
+    return rest_ensure_response($result);
 
   }
 }
