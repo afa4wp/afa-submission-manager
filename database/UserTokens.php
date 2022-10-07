12d0fe5d6c19a91c9717addc98b5bf34e0c3f8ca
claudionhangapc <claudionhangapc@gmail.com>
2022-10-07 17:26:59 -0300

    added function to create table user_tokens

---
+++ database/UserTokens.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace Database;
+
+class UserTokens{
+
+  /*
+  *
+  */
+
+  public function createTable(){
+
+    require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+    global $wpdb;
+
+    // criando a primeira tabela
+
+    $sql = "CREATE TABLE IF NOT EXISTS ".$wpdb->prefix."oi_markform_users_tokens (
+        id BIGINT(20) NOT NULL AUTO_INCREMENT,
+        user_id BIGINT(20)  UNSIGNED NOT NULL,
+        access_token VARCHAR(255) NOT NULL,
+        refresh_token VARCHAR(255) NOT NULL,
+        PRIMARY KEY(id),
+        FOREIGN KEY(user_id) REFERENCES ".$wpdb->prefix."users(ID),
+        UNIQUE(access_token),
+        UNIQUE(refresh_token)
+      )".$wpdb->get_charset_collate();
+   
+   
+       dbDelta($sql);
+
+    }
+
+
+
+  
+}
