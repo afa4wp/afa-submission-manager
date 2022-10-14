0fed1c63e011be5475dece09c6d1c749f1ed1b52
claudionhangapc <claudionhangapc@gmail.com>
2022-10-14 18:13:19 -0300

    created file to struct call all routes

---
+++ routes/Route.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Routes;
+
+use Controllers\UserController;
+use Schema\UserSchema;
+use WP_Error;
+
+class UserRoute
+{
+
+    protected $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /*
+     *
+     */
+
+    public function login()
+    {
+
+        register_rest_route(
+            $this->name,
+            '/user/login',
+            array(
+                array(
+                    'methods' => 'POST',
+                    'callback' => array(new UserController, 'login'),
+                    'permission_callback' => '__return_true',
+                    'args' => (new UserSchema())->login(),
+                ),
+
+            )
+        );
+
+    }
+
+    public function user()
+    {
+
+        register_rest_route(
+            $this->name,
+            '/user/me',
+            array(
+                array(
+                    'methods' => 'GET',
+                    'callback' => array(new UserController, 'user'),
+                    'permission_callback' => '__return_true',
+                ),
+
+            )
+        );
+
+    }
+
+    public function token()
+    {
+
+        register_rest_route(
+            $this->name,
+            '/user/token',
+            array(
+                array(
+                    'methods' => 'GET',
+                    'callback' => array(new UserController, 'token'),
+                    'permission_callback' => '__return_true',
+                ),
+
+            )
+        );
+
+    }
+
+    /*
+     *
+     */
+
+    public function initRoutes()
+    {
+        $this->login();
+        $this->user();
+        $this->token();
+    }
+
+}
