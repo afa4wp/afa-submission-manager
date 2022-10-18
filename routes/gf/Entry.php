f71d293de798de3114411cd325706bda5b030905
claudionhangapc <claudionhangapc@gmail.com>
2022-10-17 21:51:02 -0300

    created gf entry route

---
+++ routes/gf/Entry.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Routes\GF;
+
+use Controllers\GF\EntryController;
+
+
+class Entry
+{
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+	 * get all forms.
+	 */
+    public function entries()
+    {
+        register_rest_route(
+            $this->name,
+            '/gf/entries',
+            array(
+                array(
+                    'methods' => 'GET',
+                    'callback' => array(new EntryController, 'entries'),
+                    'permission_callback' => '__return_true',
+                ),
+            )
+        );
+    }
+
+
+    /**
+	 * Call all endpoints
+	 */
+    public function initRoutes()
+    {
+        $this->entries();
+    }
+
+}
