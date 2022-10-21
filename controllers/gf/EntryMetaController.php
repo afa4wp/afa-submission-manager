9a9edfe464de9ab834b19d9624e94d5bab60f247
claudionhangapc <claudionhangapc@gmail.com>
2022-10-20 22:06:37 -0300

    get entry_meta by entry id

---
+++ controllers/gf/EntryMetaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Controllers\GF;
+
+use Models\GF\EntryMetaModel;
+use WP_Error;
+
+class EntryMetaController
+{   
+    private $entryMetaModel;
+    private $number_of_records_per_page = 20;
+    public function __construct()
+    {
+        $this->entryMetaModel= new EntryMetaModel();
+         
+    }
+
+    /**
+     * GF forms entry.
+     *
+     * @param WP_REST_Request $request The request.
+     * 
+     * @return array $forms GF forms.
+     */
+    public function entryMetaByEntryID($request)
+    {   
+        $entry_id = $request['entry_id'];
+
+        $items = $this->entryMetaModel->entryMetaByEntryID($entry_id);
+      
+        return rest_ensure_response($items);
+    }
+
+    
+}
---
+++ routes/gf/EntryMeta.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Routes\GF;
+
+use Controllers\GF\EntryMetaController;
+
+
+class EntryMeta
+{
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+	 * get entry_meta by entry ID.
+	 */
+    public function entrymetaByEntryID()
+    {
+        register_rest_route(
+            $this->name,
+            '/gf/entrymeta/entry_id/(?P<entry_id>[0-9]+)',
+            array(
+                array(
+                    'methods' => 'GET',
+                    'callback' => array(new EntryMetaController, 'entryMetaByEntryID'),
+                    'permission_callback' => '__return_true',
+                ),
+            )
+        );
+    }
+
+
+    /**
+	 * Call all endpoints
+	 */
+    public function initRoutes()
+    {
+        $this->entrymetaByEntryID();
+    }
+
+}
