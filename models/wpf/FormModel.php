108475afbf58086997029a528fd9a9ef0858d86f
claudionhangapc <claudionhangapc@gmail.com>
2022-10-21 20:07:13 -0300

    created method to fetch form -wpf

---
+++ models/wpf/FormModel.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Models\WPF;
+
+use WP_Query;
+
+class FormModel
+{   
+    public const DATABASE_NAME = "pots";
+
+    public function __construct()
+    {}
+
+    /**
+	 * Get Forms 
+     * 
+     * @return array
+	 */
+    public function forms($offset, $number_of_records_per_page)
+    {
+        $posts =   new WP_Query(array(
+            'post_type'=>'wpforms',
+            'posts_per_page' =>$number_of_records_per_page,
+            'paged'=>$offset,
+            'post_status'            => array( 'publish' ),
+        ));
+
+        $forms = [];
+
+        while($posts->have_posts()){
+           
+            $posts->the_post();
+
+            $form['id'] = $posts->post->ID;
+            $form['title'] = $posts->post->post_title;
+            $form['date_created'] = $posts->post->post_date;
+            
+            $form['registers'] = wpforms()->entry->get_entries(
+                array(
+                    'form_id' => $posts->post->ID,
+                ),
+                true
+            );
+
+            $form['user_created'] = $posts->post->post_author;
+
+            $forms[] =  $form;
+        }
+
+        return $forms;
+    }
+
+    /**
+	 * Get Form by id 
+     * 
+     * @param int     $id The form ID.
+     * 
+     * @return array
+	 */
+    public function formByID($id)
+    {
+        global $wpdb;
+        $results = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix.SELF::DATABASE_NAME." WHERE id = $id ",OBJECT);
+        
+        $forms = [];
+
+        foreach($results as $key => $value){
+            
+            $form = [];
+
+            $form['id'] = $value->id;
+            $form['title'] = $value->title;
+            $form['date_created'] = $value->date_created;
+            $form['registers'] = \GFAPI::count_entries($value->id);
+            $form['user_created'] = null;
+
+            $forms[] =  $form;
+        }
+
+        if(count($forms) > 0){
+            return $forms[0];
+        }
+
+        return $forms;
+    }
+
+    /**
+	 * Get Forms 
+     * 
+     * @return int
+	 */
+    public function mumberItems()
+    {
+        return wp_count_posts('wpforms')->publish ;  
+    }
+}
