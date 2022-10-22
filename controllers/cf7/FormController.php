f1379612013854e0c843a924a3e41777c05443d2
claudionhangapc <claudionhangapc@gmail.com>
2022-10-22 20:37:41 -0300

    fetch form - cf7

---
+++ controllers/cf7/FormController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Controllers\CF7;
+
+use Models\CF7\FormModel;
+use WP_Error;
+
+class FormController
+{   
+    private $formModel;
+    private $number_of_records_per_page = 20;
+    public function __construct()
+    {
+        $this->formModel = new FormModel();
+    }
+
+    /**
+     * WEF forms.
+     *
+     * @return array $forms GF forms.
+     */
+    public function forms()
+    {   
+        $forms_results = [];
+        
+        $offset = 0;
+
+        $forms =  $this->formModel->forms($offset, $this->number_of_records_per_page);
+
+        $info = [];
+        $info["count"]  = $this->formModel->mumberItems();
+        $info["pages"]  = ceil($info["count"]/$this->number_of_records_per_page);
+        
+        $forms_results["info"] = $info;
+        $forms_results["results"] = $forms; 
+ 
+        return rest_ensure_response($forms_results); 
+    }
+
+    /**
+     * WEF forms.
+     *
+     * @param WP_REST_Request $request The request.
+     * 
+     * @return array $forms GF forms.
+     */
+    public function formsPagination($request)
+    {   
+        $page = $request['page_number'];
+
+        $forms_results = [];
+
+        $offset  = ($page - 1) * $this->formModel->mumberItems();
+
+        //$forms = \GFAPI::get_forms();
+        $forms =  $this->formModel->forms($offset, $this->number_of_records_per_page);
+
+        $info = [];
+        $info["count"]  = $this->formModel->mumberItems();
+        $info["pages"]  = ceil($info["count"]/$this->number_of_records_per_page);
+        
+        $forms_results["info"] = $info;
+        $forms_results["results"] = $forms;
+ 
+        return rest_ensure_response($forms_results);
+    }
+
+    
+}
---
+++ models/cf7/FormModel.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Models\CF7;
+
+use WP_Query;
+use Models\CF7\EntryModel;
+class FormModel
+{   
+    public const DATABASE_NAME = "posts";
+
+    public function __construct()
+    {}
+
+    /**
+	 * Get Forms 
+     * 
+     * @return array
+	 */
+    public function forms($offset, $number_of_records_per_page)
+    {
+        $posts =   new WP_Query(array(
+            'post_type'=>'wpcf7_contact_form',
+            'posts_per_page' =>$number_of_records_per_page,
+            'paged'=>$offset,
+            'post_status'            => array( 'publish' ),
+        ));
+
+        $forms = [];
+
+        while($posts->have_posts()){
+           
+            $posts->the_post();
+
+            $form['id'] = $posts->post->ID;
+            $form['title'] = $posts->post->post_title;
+            $form['date_created'] = $posts->post->post_date;
+            
+            //$form['registers'] = (new EntryModel())->mumberItemsByFormID($posts->post->ID);
+
+            $form['user_created'] = $posts->post->post_author;
+
+            $forms[] =  $form;
+        }
+
+        return $forms;
+    }
+
+    /**
+	 * Get Form by id 
+     * 
+     * @param int     $id The form ID.
+     * 
+     * @return array
+	 */
+    public function formByID($id)
+    {
+        global $wpdb;
+        $results = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix.SELF::DATABASE_NAME." WHERE id = $id ",OBJECT);
+        
+        $forms = [];
+
+        foreach($results as $key => $value){
+            
+            $form = [];
+
+            $form['id'] = $value->ID;
+            $form['title'] = $value->post_title;
+            $form['date_created'] = $value->post_date;
+            
+            $form['registers'] = (new EntryModel())->mumberItemsByFormID($value->ID);
+
+            $form['user_created'] = $value->post_author;
+
+            $forms[] =  $form;
+        }
+
+        if(count($forms) > 0){
+            return $forms[0];
+        }
+
+        return $forms;
+    }
+
+    /**
+	 * Get Forms 
+     * 
+     * @return int
+	 */
+    public function mumberItems()
+    {   
+        global $wpdb;
+        $results = $wpdb->get_results("SELECT count(*) as number_of_rows FROM ".$wpdb->prefix.SELF::DATABASE_NAME." WHERE post_type = 'wpuf_contact_form' AND post_status = 'publish' ");
+        $number_of_rows = intval( $results[0]->number_of_rows );
+        return $number_of_rows ;    
+    }
+}
