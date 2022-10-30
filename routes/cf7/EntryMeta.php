d42584d769d79ff3f46ac6a5e6c000df64d53016
claudionhangapc <claudionhangapc@gmail.com>
2022-10-30 19:03:00 -0300

    fetch entryMeta - cf7

---
+++ routes/cf7/EntryMeta.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Routes\CF7;
+
+use Controllers\CF7\EntryMetaController;
+
+
+class EntryMeta
+{
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+	 * get entry_meta by entry ID.
+	 */
+    public function entrymetaByEntryID()
+    {
+        register_rest_route(
+            $this->name,
+            '/cf7/entrymeta/entry_id/(?P<entry_id>[0-9]+)',
+            array(
+                array(
+                    'methods' => 'GET',
+                    'callback' => array(new EntryMetaController, 'entryMetaByEntryID'),
+                    'permission_callback' => '__return_true',
+                ),
+            )
+        );
+    }
+
+
+    /**
+	 * Call all endpoints
+	 */
+    public function initRoutes()
+    {
+        $this->entrymetaByEntryID();
+    }
+
+}
