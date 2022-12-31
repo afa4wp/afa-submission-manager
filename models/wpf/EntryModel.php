234218049120445200bfc43ea3e8d6dc8988607d
claudionhangapc <claudionhangapc@gmail.com>
2022-12-31 20:19:43 -0300

    added method to get entries by id

--- models/wpf/EntryModel.php
+++ models/wpf/EntryModel.php
@@ -47,6 +47,46 @@ class EntryModel
         return $entries;
     }
 
+    /**
+	 * Get Forms entries
+     * 
+     * @return array
+	 */
+    public function entryByID($entry_id)
+    {   
+        global $wpdb;
+        $results = $wpdb->get_results("SELECT * FROM ".$wpdb->prefix.SELF::DATABASE_NAME." WHERE entry_id = $entry_id ", OBJECT);
+        
+        $entries = [];
+
+        foreach($results as $key => $value){
+            
+            $entry = [];
+
+            $entry['id'] = $value->entry_id;
+            $entry['form_id'] = $value->form_id;
+            $entry['date_created'] = $value->date;
+            $entry['created_by'] = $value->user_id;
+            $entry['author_info'] = [];
+
+            if(!empty($value->user_id)){
+                $user_model = new UserModel();
+                $entry['author_info'] = $user_model->userInfoByID($value->user_id);
+            }
+
+            $form_model = new FormModel();
+            $entry['form_info'] = $form_model->formByID($value->form_id);
+            
+            $entries[] =  $entry;
+        }
+
+        if (empty($entries)){
+            return [];
+        }
+
+        return $entries[0];
+    }
+
 
     /**
 	 * Get Forms 
@@ -57,6 +97,19 @@ class EntryModel
     {
         global $wpdb;
         $results = $wpdb->get_results("SELECT count(*)  as number_of_rows FROM ".$wpdb->prefix.SELF::DATABASE_NAME."");
+        $number_of_rows = intval( $results[0]->number_of_rows );
+        return $number_of_rows ;  
+    }
+
+    /**
+	 * Get Forms 
+     * 
+     * @return int
+	 */
+    public function mumberItemsByFormID($form_id)
+    {
+        global $wpdb;
+        $results = $wpdb->get_results("SELECT count(*)  as number_of_rows FROM ".$wpdb->prefix.SELF::DATABASE_NAME." WHERE form_id = $form_id ");
         $number_of_rows = intval( $results[0]->number_of_rows );
         return $number_of_rows ;  
     }
