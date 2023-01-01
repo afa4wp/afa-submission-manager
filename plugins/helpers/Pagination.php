ac8a0f97117d02c4a8bd5f502eaba0b3ba403b3c
claudionhangapc <claudionhangapc@gmail.com>
2023-01-01 14:09:56 -0300

    create: class to handler with pagination data

---
+++ plugins/helpers/Pagination.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Plugins\Helpers;
+/**
+ * Handler with pagination info.
+ */
+
+class Pagination
+{
+    private $number_of_records_per_page = 20;
+
+    private $count ;
+
+    /**
+     * Number of item per page.
+     *
+     * @return int $number_of_records_per_page.
+     */
+    public function getNumberofRecordsPerPage(){
+        return $this->number_of_records_per_page;
+    }
+
+    /**
+     * Forms.
+     *
+     * @return array $forms.
+     */
+    public function prepareDataForRestWithPagination($count, $data){
+        
+        $this->count = $count;
+        
+        $info = [];
+        $info["count"] = $count;
+        $info["pages"]  = $this->getPages();
+
+        $data_results = [];
+
+        $data_results["info"] = $info;
+        $data_results["results"] = $data;
+
+        return  $data_results;
+    }
+
+    /**
+     * Number of pages.
+     *
+     * @return int
+     */
+    private function getPages(){
+        return  ceil($this->count/$this->number_of_records_per_page);
+    }
+
+}
