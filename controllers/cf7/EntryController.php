52b44a6333485c2e2f68c04e43e2726d343c9201
claudionhangapc <claudionhangapc@gmail.com>
2023-01-01 14:44:31 -0300

    refactor: get entries

--- controllers/cf7/EntryController.php
+++ controllers/cf7/EntryController.php
@@ -4,15 +4,18 @@ namespace Controllers\CF7;
 
 use Models\CF7\EntryModel;
 use Models\CF7\FormModel;
+use Plugins\Helpers\Pagination;
 use WP_Error;
 
 class EntryController
 {   
     private $entryModel;
-    private $number_of_records_per_page = 20;
+    private $number_of_records_per_page;
     public function __construct()
     {
         $this->entryModel = new EntryModel();
+        $this->paginationHelper = new Pagination();
+        $this->number_of_records_per_page = $this->paginationHelper->getNumberofRecordsPerPage();
     }
 
     /**
@@ -22,18 +25,13 @@ class EntryController
      */
     public function entries()
     {   
-        $entries_results = [];
+        $count = $this->entryModel->mumberItems();;
         
         $offset = 0;
 
         $entries =  $this->entryModel->entries($offset, $this->number_of_records_per_page);
 
-        $info = [];
-        $info["count"]  = $this->entryModel->mumberItems();
-        $info["pages"]  = ceil($info["count"]/$this->number_of_records_per_page);
-        
-        $entries_results["info"] = $info;
-        $entries_results["results"] = $entries;
+        $entries_results = $this->paginationHelper->prepareDataForRestWithPagination($count, $entries);
  
         return rest_ensure_response($entries_results);
     }
