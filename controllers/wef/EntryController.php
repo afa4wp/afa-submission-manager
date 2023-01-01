694ab1f145102bdda5fc0a7a076370c26571dfd6
claudionhangapc <claudionhangapc@gmail.com>
2023-01-01 17:03:31 -0300

    refactor: get entries

--- controllers/wef/EntryController.php
+++ controllers/wef/EntryController.php
@@ -3,15 +3,18 @@
 namespace Controllers\WEF;
 
 use Models\WEF\EntryModel;
+use Plugins\Helpers\Pagination;
 use WP_Error;
 
 class EntryController
 {   
     private $entryModel;
-    private $number_of_records_per_page = 20;
+    private $number_of_records_per_page;
     public function __construct()
     {
         $this->entryModel = new EntryModel();
+        $this->paginationHelper = new Pagination();
+        $this->number_of_records_per_page = $this->paginationHelper->getNumberofRecordsPerPage();
     }
 
     /**
@@ -21,18 +24,13 @@ class EntryController
      */
     public function entries()
     {   
-        $entries_results = [];
-        
+        $count = $this->entryModel->mumberItems();
+
         $offset = 0;
 
         $entries =  $this->entryModel->entries($offset, $this->number_of_records_per_page);
 
-        $info = [];
-        $info["count"]  = $this->entryModel->mumberItems();
-        $info["pages"]  = ceil($info["count"]/$this->number_of_records_per_page);
-        
-        $entries_results["info"] = $info;
-        $entries_results["results"] = $entries;
+        $entries_results = $this->paginationHelper->prepareDataForRestWithPagination($count, $entries);
  
         return rest_ensure_response($entries_results);
     }
@@ -58,20 +56,16 @@ class EntryController
     public function entriesByFormID($request)
     {   
         $form_id = $request['form_id'];
-        $page_number = $request['page_number'];
-
-        $entries_results = [];
         
-        $offset = ($page_number - 1) * $this->number_of_records_per_page;
+        $page = $request['page_number'];
+
+        $count = $this->entryModel->mumberItemsByFormID($form_id);
+        
+        $offset = $this->paginationHelper->getOffset($page, $count);
 
         $entries = $this->entryModel->entriesByFormID($form_id, $offset, $this->number_of_records_per_page);
 
-        $info = [];
-        $info["count"]  = $this->entryModel->mumberItemsByFormID($form_id);
-        $info["pages"]  = ceil($info["count"]/$this->number_of_records_per_page);
-        
-        $entries_results["info"] = $info;
-        $entries_results["results"] = $entries;
+        $entries_results = $this->paginationHelper->prepareDataForRestWithPagination($count, $entries);
  
         return rest_ensure_response($entries_results);
     }
