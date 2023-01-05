8e5a6729148257773adab299c4657e1211c5128e
claudionhangapc <claudionhangapc@gmail.com>
2023-01-04 22:09:32 -0300

    added method to get form by id

--- controllers/cf7/FormController.php
+++ controllers/cf7/FormController.php
@@ -11,13 +11,30 @@ class FormController
     private $formModel;
 
     private $number_of_records_per_page;
-
+    
+    private $paginationHelper;
+    
     public function __construct()
     {
         $this->formModel = new FormModel();
         $this->paginationHelper = new Pagination();
         $this->number_of_records_per_page = $this->paginationHelper->getNumberofRecordsPerPage();
     }
+
+    /**
+     * CF7 forms.
+     *
+     * @return object $form CF7 form.
+     */
+    public function formByID($request)
+    {   
+        $id = $request["id"];
+
+        $form =  $this->formModel->formByID($id);
+
+        return rest_ensure_response($form); 
+    }
+
 
     /**
      * CF7 forms.
