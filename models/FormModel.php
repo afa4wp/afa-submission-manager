e81ace41a29a152af505608ba563d1d8ca7dbd63
claudionhangapc <claudionhangapc@gmail.com>
2023-01-08 16:24:48 -0300

    created MainFormModel class

---
+++ models/FormModel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Models;
+
+use WP_Query;
+
+class FormModel
+{   
+    public const TABLE_NAME = "posts";
+    
+    public  $post_type ;
+
+    public function __construct($post_type)
+    {
+        $this->post_type = $post_type;
+    }
+
+    /**
+	 * Get Forms 
+     * 
+     * @return array
+	 */
+    public function forms($offset, $number_of_records_per_page)
+    {
+        $posts =   new WP_Query(array(
+            'post_type'      => $this->post_type,
+            'posts_per_page' => $number_of_records_per_page,
+            'paged'          => $offset,
+            'post_status'    => array( 'publish' ),
+        ));
+
+        return $posts;
+    }
+    
+}
