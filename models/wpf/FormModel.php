290004e9e163617bf5ba34071227ff698120ed83
claudionhangapc <claudionhangapc@gmail.com>
2023-01-08 20:02:33 -0300

    refactor: method to get forms

--- models/wpf/FormModel.php
+++ models/wpf/FormModel.php
@@ -4,16 +4,15 @@ namespace Models\WPF;
 
 use Plugins\Helpers\FormsShortcodeFinder;
 use WP_Query;
+use Models\FormModel as MainFormModel;
 
-class FormModel
+class FormModel extends MainFormModel
 {   
     public const TABLE_NAME = "posts";
-
-    private  $post_type ;
     
     public function __construct()
     {
-        $this->post_type = "wpforms";
+        parent::__construct("wpforms");
     }
 
     /**
@@ -23,35 +22,9 @@ class FormModel
 	 */
     public function forms($offset, $number_of_records_per_page)
     {
-        $posts =   new WP_Query(array(
-            'post_type'      => $this->post_type,
-            'posts_per_page' => $number_of_records_per_page,
-            'paged'          => $offset,
-            'post_status'    => array( 'publish' ),
-        ));
+        $posts = parent::forms($offset, $number_of_records_per_page);
 
-        $forms = [];
-
-        while($posts->have_posts()){
-           
-            $posts->the_post();
-
-            $form['id'] = $posts->post->ID;
-            $form['title'] = $posts->post->post_title;
-            $form['date_created'] = $posts->post->post_date;
-            
-            $form['registers'] = wpforms()->entry->get_entries(
-                array(
-                    'form_id' => $posts->post->ID,
-                ),
-                true
-            );
-
-            $form['user_created'] = $posts->post->post_author;
-            $form['perma_links'] = $this->pagesLinks($posts->post->ID);
-
-            $forms[] =  $form;
-        }
+        $forms = $this->prepareData($posts);
 
         return $forms;
     }
@@ -190,4 +163,38 @@ class FormModel
 
         return $forms;
     }
+
+    /**
+	 * Format Forms 
+     * 
+     * @return array
+	 */
+    private function prepareData($posts)
+    { 
+        $forms = [];
+
+        while($posts->have_posts()){
+           
+            $posts->the_post();
+
+            $form['id'] = $posts->post->ID;
+            $form['title'] = $posts->post->post_title;
+            $form['date_created'] = $posts->post->post_date;
+            
+            $form['registers'] = wpforms()->entry->get_entries(
+                array(
+                    'form_id' => $posts->post->ID,
+                ),
+                true
+            );
+
+            $form['user_created'] = $posts->post->post_author;
+            $form['perma_links'] = $this->pagesLinks($posts->post->ID);
+
+            $forms[] =  $form;
+        }
+
+        return $forms;
+    }
+
 }
