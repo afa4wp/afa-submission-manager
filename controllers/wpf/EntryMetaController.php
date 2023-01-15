f90875508fe723e25e4a3c2c5da36f7169bda007
claudionhangapc <claudionhangapc@gmail.com>
2023-01-14 23:21:09 -0300

    added method to search entryMeta

--- controllers/wpf/EntryMetaController.php
+++ controllers/wpf/EntryMetaController.php
@@ -29,5 +29,20 @@ class EntryMetaController
         return rest_ensure_response($items);
     }
 
+    /**
+     * WPF forms entry.
+     *
+     * @param WP_REST_Request $request The request.
+     * 
+     * @return array $forms WPF forms.
+     */
+    public function searchEntryMetaAnswer($request)
+    {   
+        $answer = urldecode($request['answer']);
+
+        $items = $this->entryMetaModel->searchEntryMetaAnswer($answer, 0, $this->number_of_records_per_page);
+      
+        return rest_ensure_response($items);
+    }
     
 }
