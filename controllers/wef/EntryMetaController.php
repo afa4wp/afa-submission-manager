83697e6a67b5340c1bbc180aac8c0c7f3081bc89
claudionhangapc <claudionhangapc@gmail.com>
2023-01-15 18:41:30 -0300

    added method to search entryMeta

--- controllers/wef/EntryMetaController.php
+++ controllers/wef/EntryMetaController.php
@@ -29,5 +29,21 @@ class EntryMetaController
         return rest_ensure_response($items);
     }
 
+    /**
+     * WEF forms entry.
+     *
+     * @param WP_REST_Request $request The request.
+     * 
+     * @return array $forms WEF forms.
+     */
+    public function searchEntryMetaAnswer($request)
+    {   
+        $answer = urldecode($request['answer']);
+
+        $items = $this->entryMetaModel->searchEntryMetaAnswer($answer, 0, 20);
+      
+        return rest_ensure_response($items);
+    }
+    
     
 }
