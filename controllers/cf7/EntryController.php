fa319244e52f4994b3bc032cd28abe5b7b69749c
claudionhangapc <claudionhangapc@gmail.com>
2023-01-17 22:41:54 -0300

    added method to search entries by user info

--- controllers/cf7/EntryController.php
+++ controllers/cf7/EntryController.php
@@ -10,11 +10,17 @@ use WP_Error;
 class EntryController
 {   
     private $entryModel;
+
     private $number_of_records_per_page;
+
+    private $paginationHelper;
+
     public function __construct()
     {
         $this->entryModel = new EntryModel();
+        
         $this->paginationHelper = new Pagination();
+        
         $this->number_of_records_per_page = $this->paginationHelper->getNumberofRecordsPerPage();
     }
 
@@ -44,7 +50,9 @@ class EntryController
     public function entryByID($request)
     {   
         $entry_id = $request['entry_id'];
+        
         $entry =  $this->entryModel->entryByID($entry_id);
+        
         return rest_ensure_response($entry);
 
     }
@@ -69,6 +77,26 @@ class EntryController
         $offset = $this->paginationHelper->getOffset($page, $count);
 
         $entries = $this->entryModel->entriesByFormID($form_id, $offset, $this->number_of_records_per_page);
+
+        $entries_results = $this->paginationHelper->prepareDataForRestWithPagination($count, $entries);
+ 
+        return rest_ensure_response($entries_results);
+    }
+
+    /**
+     * CF7 forms entries by user info.
+     *
+     * @return array $forms CF7 forms.
+     */
+    public function searchEntriesByUser($request)
+    {   
+        $user_info = $request['user_info'];
+        
+        $offset = 0;
+
+        $entries =  $this->entryModel->searchEntriesByUser($user_info, $offset, $this->number_of_records_per_page);
+
+        $count = $this->entryModel->mumberItemsByUserInfo($user_info);
 
         $entries_results = $this->paginationHelper->prepareDataForRestWithPagination($count, $entries);
  
