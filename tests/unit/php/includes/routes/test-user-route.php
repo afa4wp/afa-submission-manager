8f13e16528f65e92e58b25ad744b7bdc917f7cc5
claudionhangapc <claudionhangapc@gmail.com>
2023-01-22 21:21:58 -0300

    init to write tests

---
+++ tests/unit/php/includes/routes/test-user-route.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Class UserRouteTest
+ *
+ * @package Wp_Forms_Api
+ */
+
+use  Includes\Routes\UserRoute;
+/**
+ * userRoute test case.
+ */
+class UserRouteTest extends WP_UnitTestCase {
+
+    /**
+	 * A single example test.
+	 */
+    public function teste_init_routes() {
+        // Replace this with some actual testing code.
+        $this->assertTrue( true );
+    }
+}
