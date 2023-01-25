f6c5de6f3ac10a58c3b7e7713b55b86eb754980e
claudionhangapc <claudionhangapc@gmail.com>
2023-01-24 23:59:36 -0300

    add class to genaret QRCode

---
+++ includes/plugins/QRCode.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Includes\Plugins;
+
+use chillerlan\QRCode\QRCode as ChillerlanQRCode;
+
+class QRCode
+{   
+    private  $post_type ;
+    
+    public function __construct()
+    {
+        
+    }
+
+    public function generateQRCode(){
+
+    }
+
+    private function getData(){
+        $user_id = 1;
+
+    }
+}
