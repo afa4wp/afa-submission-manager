f76319aa1a4fba00d7c00eaefbb2b697683698d0
claudionhangapc <claudionhangapc@gmail.com>
2023-02-04 02:46:57 -0300

    save keys in option

--- includes/database/DatabaseInstaller.php
+++ includes/database/DatabaseInstaller.php
@@ -11,6 +11,20 @@ class DatabaseInstaller{
 	 */
   public function install(){
     (new UserTokens())->createTable();
+
+    $this->createSecretKeyIfIsNotDefined();
+  }
+
+  public function createSecretKeyIfIsNotDefined(){
+    
+    if (!defined('WP_AFA_ACCESS_TOKEN_SECRET_KEY') || empty(WP_AFA_ACCESS_TOKEN_SECRET_KEY)) {
+      add_option('WP_AFA_ACCESS_TOKEN_SECRET_KEY', base64_encode(openssl_random_pseudo_bytes(30)), '', false);
+    }
+
+    if (!defined('WP_AFA_REFRESH_TOKEN_SECRET_KEY') || empty(WP_AFA_REFRESH_TOKEN_SECRET_KEY)) {
+      add_option('WP_AFA_REFRESH_TOKEN_SECRET_KEY', base64_encode(openssl_random_pseudo_bytes(30)), '', false); 
+    }
+
   }
   
 }
