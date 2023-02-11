b789ee6e58b04df1c49dbc81e949d606b91bf625
claudionhangapc <claudionhangapc@gmail.com>
2023-02-11 20:26:20 -0300

    added class to hendler with admin options

--- includes/admin/AdminOptions.php
+++ includes/admin/AdminOptions.php
@@ -2,8 +2,44 @@
 
 namespace Includes\Admin;
 
-
+use Includes\Admin\Pages\UserListTable;
 class AdminOptions
 {
+    /**
+	 * WO Options Name
+	 *
+	 * @var string
+	 */
+	protected $option_name = 'wo_options';
+
+	/**
+	 * WP OAuth Server Admin Setup
+	 *
+	 * @return [type] [description]
+	 */
+	public function init() {
+        
+		//add_action( 'admin_init', array( new self(), 'admin_init' ) );
+		add_action( 'admin_menu', array( $this, 'add_page' ), 1 );
+	}
+    
+    /**
+	 * [admin_init description]
+	 *
+	 * @return [type] [description]
+	 */
+	public function admin_init() {
+		//register_setting( 'wo-options-group', $this->option_name );
+
+		// New Pages Layout
+
+	}
+    /**
+	 * [add_page description]
+	 */
+	public function add_page() {
+		add_menu_page( 'WP All Forms API', 'WP All Forms API', 'manage_options', 'wp_all_forms_api',  array(new UserListTable, 'render'), 'dashicons-rest-api');
+	}
 
 }
+
