97b8f2749cd04d16791d246a12b64f19cd414049
claudionhangapc <claudionhangapc@gmail.com>
2023-02-12 22:29:49 -0300

    send Search User data

--- includes/admin/pages/UserListTable.php
+++ includes/admin/pages/UserListTable.php
@@ -1,21 +1,37 @@
 <?php
-
 namespace Includes\Admin\Pages;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+};
 
 use Includes\Models\UserTokensModel;
 
 class UserListTable extends \WP_List_Table
 {   
+    /**
+	 * Const to declare number of posts to show per page in the table.
+	 */
+	const POSTS_PER_PAGE = 10;
+
     private $userTokensModel;
-    
+
     function __construct() {
+
         parent::__construct( array(
-       'singular'=> 'wp_list_text_link', //Singular label
-       'plural' => 'wp_list_test_links', //plural label, also this well be one of the table css class
-       'ajax'   => false //We won't support Ajax for this table
-       ) );
+            'singular'=> 'wp_all_forms_api_logged_user', //Singular label
+            'plural' => 'wp_all_forms_api_logged_users', //plural label, also this well be one of the table css class
+            'ajax'   => false //We won't support Ajax for this table
+        ) );
+
        $this->userTokensModel = new UserTokensModel();
+
      }
+
     // just the barebone implementation.
     public function get_columns() {
         $table_columns = array(
@@ -28,37 +44,23 @@ class UserListTable extends \WP_List_Table
     }
 
     public function no_items() {
-        _e( 'No users avaliable.', "" );
+        _e( 'No users logged.', "" );
     }
     
     public function prepare_items() {
         $table_data = $this->fetch_table_data();
+        $this->set_pagination_args(array(
+            "total_items" => 20,
+            "per_page" => 15
+        ));
         $this->items = $table_data;	
     }
 
-
     public function fetch_table_data() {
-        /*global $wpdb;
-        $wpdb_table = $wpdb->prefix . 'users';		
-        $orderby = ( isset( $_GET['orderby'] ) ) ? esc_sql( $_GET['orderby'] ) : 'user_registered';
-        $order = ( isset( $_GET['order'] ) ) ? esc_sql( $_GET['order'] ) : 'ASC';
-  
-        $user_query = "SELECT 
-                          user_login, display_name, user_registered, ID
-                        FROM 
-                          $wpdb_table 
-                        ORDER BY $orderby $order";
-  
-        // query output_type will be an associative array with ARRAY_A.
-        $query_results = $wpdb->get_results( $user_query, ARRAY_A  ); */
         
-        // return result array to prepare_items.
+        $query_results = $this->userTokensModel->usersTokens(0, 20);
+        return $query_results;	
 
-        $serTokensModel = new UserTokensModel();
-
-        $query_results = $serTokensModel->usersTokens(0, 20);
-
-        return $query_results;		
     }
 
     public function column_default( $item, $column_name ) {		
@@ -74,8 +76,8 @@ class UserListTable extends \WP_List_Table
 
     protected function column_cb( $item ) {
         return sprintf(		
-        '<label class="screen-reader-text" for="user_' . $item['id'] . '">' . sprintf( __( 'Select %s' ), $item['user_login'] ) . '</label>'
-        . "<input type='checkbox' name='users[]' id='user_{$item['id']}' value='{$item['id']}' />"					
+        '<label class="screen-reader-text" for="user_token_' . $item['id'] . '">' . sprintf( __( 'Select %s' ), $item['user_login'] ) . '</label>'
+        . "<input type='checkbox' name='users_tokens[]' id='user_token_{$item['id']}' value='{$item['id']}' />"					
         );
     }
 
@@ -84,16 +86,33 @@ class UserListTable extends \WP_List_Table
         $this->view();
     }
 
+    public function get_bulk_actions() {
+        $actions = array(
+            'delete' => 'Remover Usuario'
+        );
+        return $actions;
+    }
+
+    public function process_bulk_action() {
+        echo "ola: ".$this->current_action();
+        exit;
+    }
 
     public function view(){
         ?>
         <div class="wrap">    
-            <h2>WP All Forms API</h2>
+            <h2>WP All Forms API - Logged Users</h2>
                 <div id="wp-forms-api-list-table">			
-                    <div id="nds-post-body">		
+                    <div id="nds-post-body">
+                        <form id="wp-forms-api-user-search" method="get">
+                            <input type="hidden" value="wp_all_forms_api" name="page">
+                            <?php
+                                $this->search_box("Search User", "search_user_logged");
+                            ?>	 
+                        </form>		
                         <form id="wp-forms-api-user-list-form" method="get">
                             <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
-                            <?php  
+                            <?php
                                 $this->display(); 
                             ?>					
                         </form>
