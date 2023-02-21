b8add9cb9bd8093bd517c233ab87315588e5bb9a
claudionhangapc <claudionhangapc@gmail.com>
2023-02-21 18:24:06 -0300

    created screen abstract class

---
+++ includes/admin/screens/class-wp-afa-screen.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The tab screen.
+ *
+ * @package  WP_All_Forms_API
+ * @since 1.0.0
+ */
+
+namespace Includes\Admin\Screens;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class WP_AFA_Screen
+ *
+ * Render Staff content
+ *
+ * @since 1.0.0
+ */
+abstract class WP_AFA_Screen {
+
+	/**
+	 * Screen ID
+	 *
+	 * @var string
+	 */
+	protected $id;
+
+	/**
+	 * Screen label, for display
+	 *
+	 * @var string
+	 */
+	protected $label;
+
+	/**
+	 * Gets the screen ID.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_id() {
+
+		return $this->id;
+	}
+
+
+	/**
+	 * Gets the screen ID.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string
+	 */
+	public function get_label() {
+
+		return $this->label;
+	}
+
+}
