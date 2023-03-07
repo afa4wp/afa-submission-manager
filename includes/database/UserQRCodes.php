96a4238ea706683f808b499aed1def874caedc7b
claudionhangapc <claudionhangapc@gmail.com>
2023-03-06 23:48:45 -0300

    bug fix sql

--- includes/database/UserQRCodes.php
+++ includes/database/UserQRCodes.php
@@ -1,34 +1,59 @@
 <?php
+/**
+ * The User QRCOdes Class Table.
+ *
+ * @package  WP_All_Forms_API
+ * @since 1.0.0
+ */
 
 namespace Includes\Database;
 
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class UserQRCodes
+ *
+ * Create UserQRCodes Table
+ *
+ * @since 1.0.0
+ */
 class UserQRCodes {
 
-	private $dataBaseName;
-
-	public function __construct() {
-		 $this->dataBaseName = $_ENV['DATA_BASE_PREFIX'] . 'user_qr_codes';
-	}
 	/**
-	 * create fra_user_tokens table .
+	 * Data base name
+	 *
+	 * @var string
 	 */
-	public function createTable() {
+	private $data_base_name;
+
+	/**
+	 * UserQRCodes constructor.
+	 */
+	public function __construct() {
+		$this->data_base_name = $_ENV['DATA_BASE_PREFIX'] . 'user_qr_codes';
+	}
+
+	/**
+	 * Create user_qr_codes table .
+	 */
+	public function create_table() {
 
 		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 		global $wpdb;
 
-		$sql = 'CREATE TABLE IF NOT EXISTS ' . $wpdb->prefix . $this->dataBaseName . ' (
-        id BIGINT(20) NOT NULL AUTO_INCREMENT,
-        user_id BIGINT(20)  UNSIGNED NOT NULL,
-        secret VARCHAR(255) NOT NULL,
-		created_at DATETIME NOT NULL,
-		expire_secret DATETIME NOT NULL,
-        PRIMARY KEY(id),
-        FOREIGN KEY(user_id) REFERENCES ' . $wpdb->prefix . 'users(ID),
-        UNIQUE(secret),
-      )' . $wpdb->get_charset_collate();
+		$sql = 'CREATE TABLE IF NOT EXISTS ' . $wpdb->prefix . $this->data_base_name . ' (
+			id BIGINT(20) NOT NULL AUTO_INCREMENT,
+			user_id BIGINT(20)  UNSIGNED NOT NULL,
+			secret VARCHAR(255) NOT NULL,
+			expire_secret DATETIME NOT NULL,
+			created_at DATETIME NOT NULL,
+			PRIMARY KEY(id),
+			FOREIGN KEY(user_id) REFERENCES ' . $wpdb->prefix . 'users(ID),
+			UNIQUE(secret)
+		  )' . $wpdb->get_charset_collate();
 
-		 dbDelta( $sql );
+		dbDelta( $sql );
 	}
 
 }
