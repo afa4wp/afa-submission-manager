b584c8071213a0bb1f9d499ca2a16b5a6f28d4bf
claudionhangapc <claudionhangapc@gmail.com>
2023-04-09 03:01:20 -0300

    redirect on save

--- includes/admin/screens/ScreenStaff.php
+++ includes/admin/screens/ScreenStaff.php
@@ -51,7 +51,7 @@ class ScreenStaff extends Screen {
 
 		?>	
 			<div>
-			Scan the following QR Code using the WP AFA app.
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit.
 			</div>
 			<form action="<?php echo esc_html( admin_url( 'admin.php?page=wp_all_forms_api_settings&tab=staff' ) ); ?>" method="POST">
 				<table class="form-table">
@@ -75,15 +75,19 @@ class ScreenStaff extends Screen {
 	 * @return void
 	 */
 	public function settings() {
+		$all_options = get_option( 'wp_all_forms_api_settings_staff_options', false );
+
+		if ( empty( $all_options ) ) {
+			$all_options = array();
+		}
+
 		add_settings_field(
 			'wp_all_forms_api_add_user',
 			'Adicionar usuarios',
 			array( $this, 'input_add_user_render' ),
 			'wp_all_forms_api_settings',
 			'wp_all_forms_api_settings_staff_section',
-			array(
-				'value' => 'teste',
-			)
+			$all_options
 		);
 	}
 
@@ -97,15 +101,19 @@ class ScreenStaff extends Screen {
 	 * @return void
 	 */
 	public function input_add_user_render( array $args ) {
-		$value = '';
-		if ( array_key_exists( 'value', $args ) ) {
-			$value = $args['value'];
+
+		$checked = '';
+		if ( array_key_exists( 'add_user', $args ) ) {
+			$value = $args['add_user'];
+			if ( 'on' === $value ) {
+				$checked = 'checked';
+			}
 		}
 
 		?>
 			<fieldset>
 				<label for="wp_all_forms_api_add_user">
-					<input name="wp_all_forms_api_add_user" id="wp_all_forms_api_add_user" type="checkbox" class="" value="1" > 
+					<input name="wp_all_forms_api_add_user" id="wp_all_forms_api_add_user" type="checkbox" class="" value="on" <?php echo esc_html( $checked ); ?>> 
 					Adiciona novo usarios.	
 				</label> 
 				<p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero fuga maxime.</p>
@@ -125,11 +133,15 @@ class ScreenStaff extends Screen {
 		if ( isset( $_REQUEST['_wpnonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ), 'sreen-staff' ) ) {
 
 			if ( ! empty( $_POST['wp_all_forms_api_add_user'] ) ) {
-				$add_user = sanitize_text_field( wp_unslash( $_POST['wp_all_forms_api_add_user'] ) );
+				$add_user                = sanitize_text_field( wp_unslash( $_POST['wp_all_forms_api_add_user'] ) );
 				$all_options['add_user'] = $add_user;
 			}
 
-			update_option('wp_all_forms_api_settings_staff_options', $allOptions );
+			update_option( 'wp_all_forms_api_settings_staff_options', $all_options );
+
+			if ( wp_safe_redirect( admin_url( 'admin.php?page=wp_all_forms_api_settings&tab=staff' ) ) ) {
+				exit;
+			}
 		}
 
 	}
