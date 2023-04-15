19491bec029b4f04d974edced4c89a99bc406947
claudionhangapc <claudionhangapc@gmail.com>
2023-04-14 22:30:08 -0300

    added method to search logged users

--- includes/controllers/UserTokensController.php
+++ includes/controllers/UserTokensController.php
@@ -51,16 +51,37 @@ class UserTokensController {
 	 *
 	 * @return array $UserTokensModel the items.
 	 */
-	public function paginate( $page = 0 ) {
+	public function paginate( $page = 1 ) {
 
 		$count                      = $this->user_tokens_model->mumber_items();
 		$number_of_records_per_page = $this->pagination_helper->getNumberofRecordsPerPage();
-		$items                      = $this->user_tokens_model->users_tokens( $page, $number_of_records_per_page );
+		$offset                     = $this->pagination_helper->getOffset( $page, $count );
+		$items                      = $this->user_tokens_model->users_tokens( $offset, $number_of_records_per_page );
 
 		$results = $this->pagination_helper->prepareDataForRestWithPagination( $count, $items );
 
 		return $results;
 	}
 
+	/**
+	 * Get UserTokensController items by display_name, user_login or user_email.
+	 *
+	 * @param string $user_info The user info.
+	 * @param int    $page The page.
+	 *
+	 * @return array $UserTokensModel the items.
+	 */
+	public function search( $user_info, $page = 1 ) {
+
+		$count = $this->user_tokens_model->mumber_items_search( $user_info );
+
+		$number_of_records_per_page = $this->pagination_helper->getNumberofRecordsPerPage();
+		$offset                     = $this->pagination_helper->getOffset( $page, $count );
+		$items                      = $this->user_tokens_model->search_users_tokens( $user_info, $offset, $number_of_records_per_page );
+
+		$results = $this->pagination_helper->prepareDataForRestWithPagination( $count, $items );
+
+		return $results;
+	}
 
 }
