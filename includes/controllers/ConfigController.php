df7e0a972c29d407473f5eea950f3ce079e6319b
claudionhangapc <claudionhangapc@gmail.com>
2023-05-21 18:13:11 -0300

    added ConfigController file

---
+++ includes/controllers/ConfigController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The Config Controller Class.
+ *
+ * @package  WP_All_Forms_API
+ * @since 1.0.0
+ */
+
+namespace Includes\Controllers;
+
+use Includes\Plugins\Config;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class ConfigController
+ *
+ * Manipulate User info
+ *
+ * @since 1.0.0
+ */
+class ConfigController {
+
+	/**
+	 * Config Plugins
+	 *
+	 * @var Config
+	 */
+	private $config;
+
+	/**
+	 * ConfigController constructor.
+	 */
+	public function __construct() {
+		$this->config = new Config();
+	}
+
+	/**
+	 * Get activated foms.
+	 *
+	 * @param WP_REST_Request $request The request.
+	 *
+	 * @return array Installed plugin.
+	 */
+	public function forms( $request ) {
+		return rest_ensure_response( $this->config->installed_forms() );
+	}
+
+}
