ba7d5ca9c6ac4341b557e487240a143f8311151a
claudionhangapc <claudionhangapc@gmail.com>
2023-07-29 18:20:11 -0300

    added class to loads notifications hooks

---
+++ includes/plugins/notificication/NotificationHooksPlugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The Notification Hooks Plugin Class.
+ *
+ * @package  WP_All_Forms_API
+ * @since 1.0.0
+ */
+
+namespace Includes\Plugins\Notification;
+
+use Includes\Plugins\Notification\CF7Notification;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class NotificationHooksPlugin
+ *
+ * Load Notifications Hooks
+ *
+ * @since 1.0.0
+ */
+class NotificationHooksPlugin {
+
+	/**
+	 * Load hooks for notifications
+	 *
+	 * @return void
+	 */
+	public function loads_hooks() {
+		( new CF7Notification() )->loads_hooks();
+	}
+
+
+}
