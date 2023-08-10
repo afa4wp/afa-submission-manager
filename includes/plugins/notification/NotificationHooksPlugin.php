101c62e92b85090839935b7254b2c3701f6b9c66
claudionhangapc <claudionhangapc@gmail.com>
2023-08-10 20:37:57 -0300

    created class to hendle with wpf notification

--- includes/plugins/notification/NotificationHooksPlugin.php
+++ includes/plugins/notification/NotificationHooksPlugin.php
@@ -10,6 +10,7 @@ namespace Includes\Plugins\Notification;
 
 use Includes\Plugins\Notification\CF7Notification;
 use Includes\Plugins\Notification\GFNotification;
+use Includes\Plugins\Notification\WPFNotification;
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
@@ -30,6 +31,7 @@ class NotificationHooksPlugin {
 	 */
 	public function loads_hooks() {
 		( new CF7Notification() )->loads_hooks();
+		( new WPFNotification() )->loads_hooks();
 		( new GFNotification() )->loads_hooks();
 	}
 
