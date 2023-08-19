8b328ebf0e4cfc9e65e7a1ec9d6312ad7b9d9638
claudionhangapc <claudionhangapc@gmail.com>
2023-08-19 15:39:45 -0300

    return default gravatar

--- includes/models/UserModel.php
+++ includes/models/UserModel.php
@@ -111,7 +111,7 @@ class UserModel {
 		$user_info['user_id']    = $user->ID;
 		$user_info['user_name']  = $user->display_name;
 		$user_info['user_email'] = $user->user_email;
-		$user_info['avatar_url'] = get_avatar_url( $user_id, array( 'default' => 'wavatar' ) );
+		$user_info['avatar_url'] = get_avatar_url( $user_id );
 
 		return $user_info;
 	}
